104f9183771c33fa66724c7e8b864c8c9b250976
Glazelf <gideon.steenbergen@gmail.com>
2020-11-03 22:05:41 +0100

    add disclaimer that this command is WIP

--- commands/games/rental.js
+++ commands/games/rental.js
@@ -3,7 +3,10 @@ module.exports.run = async (client, message) => {
     try {
         const Discord = require("discord.js");
 
-        return;
+        const args = message.content.split(' ');
+        let pokemonSearch = args[1];
+
+        return message.channel.send(`> This command isn't finished yet, ${message.author}.`);
 
     } catch (e) {
         // log error
